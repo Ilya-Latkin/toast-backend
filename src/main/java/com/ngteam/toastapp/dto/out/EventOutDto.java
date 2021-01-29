2148a73a5febff42486312e6aeb12b7f194a451c
Ilya-Latkin <latkin.ilia@gmail.com>
2021-01-29 11:47:15 +0300

    out dto folder returned

---
+++ src/main/java/com/ngteam/toastapp/dto/out/EventOutDto.java
@@ -0,0 +1,19 @@
+package com.ngteam.toastapp.dto.out;
+
+import com.ngteam.toastapp.dto.in.CategoryDto;
+import com.ngteam.toastapp.dto.in.EventTypeDto;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Setter
+@Getter
+public class EventOutDto {
+    private long id;
+    private String name;
+    private String description;
+    private Date date;
+    private EventTypeDto eventType;
+    private CategoryDto category;
+}
